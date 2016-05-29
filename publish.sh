1180a1f64b1ba6a6499ce4c7ff1177aa21057dc9
Amit Saha <amitsaha.in@gmail.com>
2016-05-28 19:22:50 -0700

    Update

--- publish.sh
+++ publish.sh
@@ -3,10 +3,12 @@
 jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
 mv slides.slides.html  index.html
 cp index.html /tmp/
+cp -r images /tmp/
 git add -A .
 git commit -m "Update"
 git checkout gh-pages
 mv /tmp/index.html .
+cp -r /tmp/images images/
 git add -A .
 git commit -m "new version"
 git push origin master gh-pages
